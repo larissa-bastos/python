6263585f8ab409f00eabc56841b49961ac3c1170
Larissa Bastos Rocha <103389141+larissa-bastos@users.noreply.github.com>
2022-04-25 23:29:49 -0300

    Initial commit

---
+++ desafio 3.py
@@ -0,0 +1,4 @@
+numero1 = input ('Me diga um número')
+numero2 = input ('Me diga mais um número')
+numero3 = input (numero1 + numero2)
+print ('A soma é', numero3)
---
+++ teste.py
@@ -0,0 +1,4 @@
+nome = input('Qual o seu nome?')
+idade = input('Qual a sua idade?')
+profissão = input ('Qual a sua profissão?')
+print(nome, idade, profissão)
